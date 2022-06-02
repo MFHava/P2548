d6cb1a446cb0b46182b7d4750cd50f2fe62cbd93
MFHava <mfh@live.at>
2022-06-02 14:25:44 +0200

    started work on copyable_function  * renamed namespaces ptl -> p2548  * renamed class function -> copyable_function  * fixed all TODO: [C++20] comments  * added TODOs for missing copying support

--- inc/copyable_function.hpp
+++ inc/copyable_function.hpp
@@ -8,8 +8,8 @@
 #include <utility>
 #include <type_traits>
 
-namespace ptl {
-	namespace internal_function {
+namespace p2548 {
+	namespace internal {
 		enum class refness { none, lvalue, rvalue, };
 
 		using none_type = std::integral_constant<refness, refness::none>;
@@ -145,11 +145,8 @@ namespace ptl {
 		bool is_in_place_type_t_specialization_v{is_in_place_type_t_specialization<T>::value};
 
 
-		template<typename T>
-		using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>; //TODO: [C++20] replace with std::remove_cvref_t
-
-
-		template<typename R, typename F, typename... Args, typename = std::enable_if_t<std::is_invocable_r_v<R, F, Args...>>> //TODO: [C++20] replace with concepts/requires-clause
+		template<typename R, typename F, typename... Args>
+		requires std::is_invocable_r_v<R, F, Args...>
 		constexpr
 		auto invoke_r(F && f, Args &&... args) noexcept(std::is_nothrow_invocable_r_v<R, F, Args...>) { //TODO: [C++23] replace with std::invoke_r
 			if constexpr(std::is_void_v<R>) std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
@@ -157,17 +154,17 @@ namespace ptl {
 		}
 	}
 
-	//! @brief move-only function wrapper
+	//! @brief copyable function wrapper
 	//! @tparam Signature function signature of the contained functor (including potential const-, ref- and noexcept-qualifiers)
-	template<typename Signature, typename = typename internal_function::traits<Signature>::function>
-	class function;
+	template<typename Signature, typename = typename internal::traits<Signature>::function>
+	class copyable_function;
 
-	namespace internal_function {
+	namespace internal {
 		template<typename...>
 		struct is_function_specialization : std::false_type {};
 
 		template<typename... Ts>
-		struct is_function_specialization<function<Ts...>> : std::true_type {};
+		struct is_function_specialization<copyable_function<Ts...>> : std::true_type {};
 
 		template<typename... Ts>
 		inline
@@ -176,32 +173,37 @@ namespace ptl {
 	}
 
 	template<typename Signature, typename Result, typename... Args>
-	class function<Signature, Result(Args...)> final {
-		using traits = internal_function::traits<Signature>;
+	class copyable_function<Signature, Result(Args...)> final {
+		using traits = internal::traits<Signature>;
 
 		using ref_ = typename traits::ref;
 
 		static
 		constexpr
-		bool noexcept_{typename traits::noexcept_{}};
+		bool const_{typename traits::const_{}},
+		     noexcept_{typename traits::noexcept_{}},
+		     none{ref_{} == internal::refness::none},
+		     lvalue{ref_{} == internal::refness::lvalue},
+		     rvalue{ref_{} == internal::refness::rvalue};
 
 		template<typename T>
-		using add_const = std::conditional_t<typename traits::const_{}, const T, T>;
+		using add_const = std::conditional_t<const_, const T, T>;
 
 		template<typename T>
-		using add_quals = internal_function::add_refness_t<add_const<T>, ref_::value>;
+		using add_quals = internal::add_refness_t<add_const<T>, ref_::value>;
 
 		template<typename T>
-		using add_inv_quals = internal_function::add_refness_t<add_const<T>, ref_::value == internal_function::refness::none ? internal_function::refness::lvalue : ref_::value>;
+		using add_inv_quals = internal::add_refness_t<add_const<T>, ref_::value == internal::refness::none ? internal::refness::lvalue : ref_::value>;
 
 		union storage_t {
 			void * ptr;
 			char sbo[sizeof(void * ) * 3];
 		};
 		const struct vtable final {
-			auto (*manage)(storage_t *, storage_t *, bool) noexcept -> bool;
+			auto (*manage)(storage_t *, storage_t *, bool) noexcept -> bool; //TODO: add copying? OR introduce new copy-operation...
 			auto (*dispatch)(add_const<storage_t> *, Args...) noexcept(noexcept_) -> Result;
 
+			//TODO: copy based on manage?
 			void dtor(storage_t * self) const noexcept { manage(self, nullptr, false); }
 			auto move(storage_t * from, storage_t * to) const noexcept -> bool { return manage(from, to, false); } //returns true => heap-allocated object was moved; from is now empty => storage & vptr must be reset...
 			void destructive_move(storage_t * from, storage_t * to) const noexcept { manage(from, to, true); }
@@ -210,13 +212,15 @@ namespace ptl {
 
 		template<typename T, bool SBO>
 		static
-		auto invoke(add_const<storage_t> * ctx, Args... args) noexcept(noexcept_) -> Result { return internal_function::invoke_r<Result>(static_cast<add_inv_quals<T>>(*reinterpret_cast<add_const<T> *>(SBO ? ctx->sbo : ctx->ptr)), std::forward<Args>(args)...); }
+		auto invoke(add_const<storage_t> * ctx, Args... args) noexcept(noexcept_) -> Result { return internal::invoke_r<Result>(static_cast<add_inv_quals<T>>(*reinterpret_cast<add_const<T> *>(SBO ? ctx->sbo : ctx->ptr)), std::forward<Args>(args)...); }
 
 		template<typename T, typename... A>
 		void init(A &&... args) {
 			static_assert(std::is_nothrow_destructible_v<T>);
+			//TODO: static_assert(std::is_copy_constructible_v<T>);
+			//TODO: static_assert(std::is_copy_assignable_v<T>);
 
-			if constexpr(constexpr auto sbo{sizeof(T) <= sizeof(storage_t::sbo) && std::is_nothrow_move_constructible_v<T>}; sbo) {
+			if constexpr(constexpr auto sbo{sizeof(T) <= sizeof(storage_t::sbo) && std::is_nothrow_move_constructible_v<T>}; sbo) { //TODO: must be nothrow_copy_constructible unless SBO will be extremely problematic
 				static constexpr vtable vtable{
 					+[](storage_t * from, storage_t * to, bool destructive) noexcept {
 						if(to) new(to->sbo) T{std::move(*reinterpret_cast<T *>(from->sbo))};
@@ -258,14 +262,15 @@ namespace ptl {
 		bool is_callable_from{noexcept_ ? std::is_nothrow_invocable_r_v<Result, add_quals<VT>, Args...> && std::is_nothrow_invocable_r_v<Result, add_inv_quals<VT>, Args...>
 		                                : std::is_invocable_r_v<Result, add_quals<VT>, Args...> && std::is_invocable_r_v<Result, add_inv_quals<VT>, Args...>};
 	public:
-		function() noexcept { init_empty(); }
-		function(std::nullptr_t) noexcept : function{} {}
+		copyable_function() noexcept { init_empty(); }
+		copyable_function(std::nullptr_t) noexcept : copyable_function{} {}
 
-		template<typename F, typename = std::enable_if_t<!std::is_same_v<function, internal_function::remove_cvref_t<F>> && !internal_function::is_in_place_type_t_specialization_v<internal_function::remove_cvref_t<F>> && is_callable_from<std::decay_t<F>>>> //TODO: [C++20] replace with concepts/requires-clause
-		function(F && func) {
+		template<typename F>
+		requires(!std::is_same_v<copyable_function, std::remove_cvref_t<F>> && !internal::is_in_place_type_t_specialization_v<std::remove_cvref_t<F>> && is_callable_from<std::decay_t<F>>)
+		copyable_function(F && func) {
 			using VT = std::decay_t<F>;
 			static_assert(std::is_constructible_v<VT, F>);
-			if constexpr(std::is_function_v<std::remove_pointer_t<F>> || std::is_member_pointer_v<F> || internal_function::is_function_specialization_v<internal_function::remove_cvref_t<F>>)
+			if constexpr(std::is_function_v<std::remove_pointer_t<F>> || std::is_member_pointer_v<F> || internal::is_function_specialization_v<std::remove_cvref_t<F>>)
 				if(!func) {
 					init_empty();
 					return;
@@ -273,17 +278,22 @@ namespace ptl {
 			init<VT>(std::forward<F>(func));
 		}
 
-		template<typename T, typename... A, typename = std::enable_if_t<std::is_constructible_v<std::decay_t<T>, A &&...> && is_callable_from<std::decay_t<T>>>> //TODO: [C++20] replace with concepts/requires-clause
+		template<typename T, typename... A>
+		requires(std::is_constructible_v<std::decay_t<T>, A &&...> && is_callable_from<std::decay_t<T>>)
 		explicit
-		function(std::in_place_type_t<T>, A &&... args) {
+		copyable_function(std::in_place_type_t<T>, A &&... args) {
 			using VT = std::decay_t<T>;
 			static_assert(std::is_same_v<VT, std::decay_t<T>>);
 			init<VT>(std::forward<A>(args)...);
 		}
 
-		function(function && other) noexcept : vptr{other.vptr} { if(other.vptr->move(&other.storage, &storage)) other.init_empty(); }
+		//TODO: copy-ctor
 
-		auto operator=(function && other) noexcept -> function & {
+		copyable_function(copyable_function && other) noexcept : vptr{other.vptr} { if(other.vptr->move(&other.storage, &storage)) other.init_empty(); }
+
+		//TODO: copy-assign
+
+		auto operator=(copyable_function && other) noexcept -> copyable_function & {
 			if(this != &other) {
 				vptr->dtor(&storage);
 				vptr = other.vptr;
@@ -291,36 +301,30 @@ namespace ptl {
 			}
 			return *this;
 		}
-		auto operator=(std::nullptr_t) noexcept -> function & {
-			if(*this) function{}.swap(*this);
+		auto operator=(std::nullptr_t) noexcept -> copyable_function & {
+			if(*this) copyable_function{}.swap(*this);
 			return *this;
 		}
 
 		template<typename F>
-		auto operator=(F && func) -> function & {
-			function{std::forward<F>(func)}.swap(*this);
+		auto operator=(F && func) -> copyable_function & {
+			copyable_function{std::forward<F>(func)}.swap(*this);
 			return *this;
 		}
 
-		~function() noexcept { vptr->dtor(&storage); }
+		~copyable_function() noexcept { vptr->dtor(&storage); }
 
 		explicit
 		operator bool() const noexcept { return vptr->dispatch; }
 
-		template<typename T = internal_function::traits<Signature>>
-		auto operator()(Args... args, std::enable_if_t<!T::const_::value && T::ref::value == internal_function::refness::none, int> = 0) noexcept(noexcept_) -> Result { return vptr->dispatch(&storage, std::forward<Args>(args)...); } //TODO: [C++20] replace with concepts/requires-clause
-		template<typename T = internal_function::traits<Signature>>
-		auto operator()(Args... args, std::enable_if_t<T::const_::value && T::ref::value == internal_function::refness::none, int> = 0) const noexcept(noexcept_) -> Result { return vptr->dispatch(&storage, std::forward<Args>(args)...); } //TODO: [C++20] replace with concepts/requires-clause
-		template<typename T = internal_function::traits<Signature>>
-		auto operator()(Args... args, std::enable_if_t<!T::const_::value && T::ref::value == internal_function::refness::lvalue, int> = 0) & noexcept(noexcept_) -> Result { return vptr->dispatch(&storage, std::forward<Args>(args)...); } //TODO: [C++20] replace with concepts/requires-clause
-		template<typename T = internal_function::traits<Signature>>
-		auto operator()(Args... args, std::enable_if_t<T::const_::value && T::ref::value == internal_function::refness::lvalue, int> = 0) const & noexcept(noexcept_) -> Result { return vptr->dispatch(&storage, std::forward<Args>(args)...); } //TODO: [C++20] replace with concepts/requires-clause
-		template<typename T = internal_function::traits<Signature>>
-		auto operator()(Args... args, std::enable_if_t<!T::const_::value && T::ref::value == internal_function::refness::rvalue, int> = 0) && noexcept(noexcept_) -> Result { return vptr->dispatch(&storage, std::forward<Args>(args)...); } //TODO: [C++20] replace with concepts/requires-clause
-		template<typename T = internal_function::traits<Signature>>
-		auto operator()(Args... args, std::enable_if_t<T::const_::value && T::ref::value == internal_function::refness::rvalue, int> = 0) const && noexcept(noexcept_) -> Result { return vptr->dispatch(&storage, std::forward<Args>(args)...); } //TODO: [C++20] replace with concepts/requires-clause
+		auto operator()(Args... args) noexcept(noexcept_) -> Result requires(!const_ && none) { return vptr->dispatch(&storage, std::forward<Args>(args)...); }
+		auto operator()(Args... args) const noexcept(noexcept_) -> Result requires(const_ && none) { return vptr->dispatch(&storage, std::forward<Args>(args)...); }
+		auto operator()(Args... args) & noexcept(noexcept_) -> Result requires(!const_ && lvalue) { return vptr->dispatch(&storage, std::forward<Args>(args)...); }
+		auto operator()(Args... args) const & noexcept(noexcept_) -> Result requires(const_ && lvalue) { return vptr->dispatch(&storage, std::forward<Args>(args)...); }
+		auto operator()(Args... args) && noexcept(noexcept_) -> Result requires(!const_ && rvalue) { return vptr->dispatch(&storage, std::forward<Args>(args)...); }
+		auto operator()(Args... args) const && noexcept(noexcept_) -> Result requires(const_ && rvalue) { return vptr->dispatch(&storage, std::forward<Args>(args)...); }
 
-		void swap(function & other) noexcept {
+		void swap(copyable_function & other) noexcept {
 			if(this == &other) return;
 			storage_t tmp;
 			vptr->destructive_move(&storage, &tmp);
@@ -329,15 +333,9 @@ namespace ptl {
 			std::swap(vptr, other.vptr);
 		}
 		friend
-		void swap(function & lhs, function & rhs) noexcept { lhs.swap(rhs); }
+		void swap(copyable_function & lhs, copyable_function & rhs) noexcept { lhs.swap(rhs); }
 
 		friend
-		auto operator==(const function & self, std::nullptr_t) noexcept -> bool { return !self; }
-		friend
-		auto operator==(std::nullptr_t, const function & self) noexcept -> bool { return !self; } //TODO: [C++20] remove as implicitly generated
-		friend
-		auto operator!=(const function & self, std::nullptr_t) noexcept -> bool { return static_cast<bool>(self); } //TODO: [C++20] remove as implicitly generated
-		friend
-		auto operator!=(std::nullptr_t, const function & self) noexcept -> bool { return static_cast<bool>(self); } //TODO: [C++20] remove as implicitly generated
+		auto operator==(const copyable_function & self, std::nullptr_t) noexcept -> bool { return !self; }
 	};
 }
